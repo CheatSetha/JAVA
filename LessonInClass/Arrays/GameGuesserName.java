1bd8adb0974c5d74632d03d414e45d7d7ddb6b55
CheatSetha <cheatsetha2001@gmail.com>
2023-02-28 19:32:04 +0700

    class practise

--- LessonInClass/Arrays/GameGuesserName.java
+++ LessonInClass/Arrays/GameGuesserName.java
@@ -6,39 +6,48 @@ import java.util.Scanner;
 public class GameGuesserName {
     public static void main (String[] args) {
         String input;//to hold the use input
-        Scanner userInput = new Scanner(System.in);
-        System.out.println("Enter your name : ");
-        input = userInput.nextLine().toLowerCase();
-        int sum = 0;
-        int number = 0;
+        char check; // to hold that user want to check again or not
+        do {
+            Scanner userInput = new Scanner(System.in);
+            System.out.println("Enter your name : ");
+            input = userInput.nextLine().toLowerCase();
+            int sum = 0;
+            int number = 0;
 
-        for (int i = 0; i < input.length(); i++) {
-            switch (input.charAt(i)) {
-                case 'a', 'j', 's' -> number += 1;
-                case 'k', 'b', 't' -> number += 2;
-                case 'c', 'l', 'u' -> number += 3;
-                case 'd', 'm', 'v' -> number += 4;
-                case 'e', 'n', 'w' -> number += 5;
-                case 'f', 'o', 'x' -> number += 6;
-                case 'g', 'p', 'y' -> number += 7;
-                case 'h', 'q', 'z' -> number += 8;
-                case 'i', 'r' -> number += 9;
-                default -> number += 0;
+            for (int i = 0; i < input.length(); i++) {
+                switch (input.charAt(i)) {
+                    case 'a', 'j', 's' -> number += 1;
+                    case 'k', 'b', 't' -> number += 2;
+                    case 'c', 'l', 'u' -> number += 3;
+                    case 'd', 'm', 'v' -> number += 4;
+                    case 'e', 'n', 'w' -> number += 5;
+                    case 'f', 'o', 'x' -> number += 6;
+                    case 'g', 'p', 'y' -> number += 7;
+                    case 'h', 'q', 'z' -> number += 8;
+                    case 'i', 'r' -> number += 9;
+                    default -> number += 0;
 
+                }
             }
-        }
 
-        if (number>10){
-            //convert to char
-          char[] chars = String.valueOf(number).toCharArray();
-            for (int i = 0; i <chars.length ; i++) {
+            if (number>10){
+                //convert to char
+                char[] chars = String.valueOf(number).toCharArray();
+                for (int i = 0; i <chars.length ; i++) {
 
 
-                sum=sum+Character.getNumericValue(chars[i]); //convert to int and calculate
+                    sum=sum+Character.getNumericValue(chars[i]); //convert to int and calculate
 
+                }
+                System.out.println("អ្នកជាមនុស្សជំពូកលេខ: " +sum);
             }
-            System.out.println("អ្នកជាមនុស្សជំពូកលេខ: " +sum);
-        }
+
+            System.out.println("Do You want to check again? press y for yes : ");
+             check = userInput.nextLine().charAt(0);
+
+
+
+        }while (check=='y' || check=='Y');
 
     }
 }
